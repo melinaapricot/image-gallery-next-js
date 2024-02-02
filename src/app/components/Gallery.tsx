a0e3ada3cba548f311f848db6036c1472b01ce29
melinaapricot <melinaverikios@gmail.com>
2024-02-01 18:22:14 -0600

    add blur image urls

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -1,6 +1,7 @@
 import fetchImages from "@/lib/fetchImages";
 import ImgContainer from "./ImgContainer";
 import type { ImagesResults } from "@/models/Images";
+import addBlurredDataUrls from "@/lib/getBase64";
 
 export default async function Gallery() {
 
@@ -8,11 +9,12 @@ export default async function Gallery() {
     const images: ImagesResults | undefined = await fetchImages(url)
 
     if (!images) return <h2 className="m-4 text-2xl font-bold">No Images Found</h2>
+    const photosWithBlur = await addBlurredDataUrls(images)
 
     return (
         <section className="px-2 my-3 grid gap-2 grid-cols-gallery">
-                {images.photos.map(photo => (
-                    <ImgContainer photo={photo}/>
+                {photosWithBlur.map(photo => (
+                    <ImgContainer key={photo.id} photo={photo}/>
                 ))}
         </section>
     )
